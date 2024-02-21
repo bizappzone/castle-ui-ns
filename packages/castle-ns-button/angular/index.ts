2e8b6782b8322166c85e753d88d6713aae730480
AJ <aj@bizappzone.com>
2024-02-21 12:53:49 -0500

    baseline

---
+++ packages/castle-ns-button/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { CastleNsButton } from '@bizappzone/castle-ns-button';
+
+@NgModule()
+export class NativeScriptCastleNsButtonModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('CastleNsButton', () => CastleNsButton);
---
+++ packages/castle-ns-icon/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { CastleNsIcon } from '@bizappzone/castle-ns-icon';
+
+@NgModule()
+export class NativeScriptCastleNsIconModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('CastleNsIcon', () => CastleNsIcon);
